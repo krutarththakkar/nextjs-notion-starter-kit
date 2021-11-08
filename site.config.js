6aa73ef17dfe7b84ebe0f0b7a39b0718ea5e58a9
Krutarth <35326083+krutarththakkar@users.noreply.github.com>
2021-11-09 00:48:05 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'Krutarth',
-  domain: 'studybuddy.courses',
+  domain: 'www.studybuddy.courses',
   author: 'Krutarth',
 
   // open graph metadata (optional)
